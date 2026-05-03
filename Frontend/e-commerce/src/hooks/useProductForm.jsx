5b3308ef14f8f6a6d3e181753e2678b5f2e9929c
AfridKhan <k0786khan786@gmail.com>
2026-05-03 15:50:07 +0545

    Fixed zod

--- Frontend/e-commerce/src/hooks/useProductForm.jsx
+++ Frontend/e-commerce/src/hooks/useProductForm.jsx
@@ -1,6 +1,5 @@
 import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { productSchema } from "../../../../Shared/Schema/ProductSchema";
+import { productSchema } from "../components/shared/ProductSchema";
 export default function useProductForm() {
 
     return useForm({
