b5dc94d244d1433f40f73428a93facee4af96fe1
AfridKhan <k0786khan786@gmail.com>
2026-05-04 13:18:01 +0545

    Fixed request Url redux

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const dashboardStats = require("./routes/dashboardRoute.js");
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 
