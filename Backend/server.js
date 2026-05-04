21c829e0a70ac9ac1801cf0491e2d7c29300d34b
AfridKhan <k0786khan786@gmail.com>
2026-05-04 16:46:39 +0545

    Fixing Dev CORS issue

--- Backend/server.js
+++ Backend/server.js
@@ -19,11 +19,24 @@ const bannerRoute = require("./routes/bannerRoutes.js");
 const dashboardStats = require("./routes/dashboardRoute.js");
 
 // Middleware
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://shopsmart-by-afrid.vercel.app"
+];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS: " + origin));
+    },
     credentials: true
 }));
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
