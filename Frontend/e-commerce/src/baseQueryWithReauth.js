293a2b8ded86871f77fbab7264d4c26fa16ee797
AfridKhan <k0786khan786@gmail.com>
2026-06-22 19:51:04 +0545

    Fixed Ui issues

--- Frontend/e-commerce/src/baseQueryWithReauth.js
+++ Frontend/e-commerce/src/baseQueryWithReauth.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: import.meta.env.VITE_API_URL + "/api",
   credentials: 'include',
 });
 
