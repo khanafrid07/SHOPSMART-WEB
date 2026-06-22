1d339669fdb0737d2e3c20ea8a9c4dc912c0007e
AfridKhan <k0786khan786@gmail.com>
2026-06-22 17:58:57 +0545

    FIxed deployemet

--- Frontend/e-commerce/src/baseQueryWithReauth.js
+++ Frontend/e-commerce/src/baseQueryWithReauth.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: import.meta.env.VITE_API_URL + "/api",
   credentials: 'include',
 });
 
